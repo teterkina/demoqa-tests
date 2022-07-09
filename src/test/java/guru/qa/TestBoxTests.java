5fb3210968f3c19d4cfbd7d8af749396c4a5e398
teterkina <teterkina@digitaled.ru>
2022-07-09 12:26:35 +0300

    added selenide test

---
+++ src/test/java/guru/qa/TestBoxTests.java
@@ -0,0 +1,35 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successTest() {
+        open("https://demoqa.com/text-box");
+
+        $("#userName").setValue("Alina");
+        $("#userEmail").setValue("test@email.com");
+        $("#currentAddress").setValue("Some address");
+        $("#permanentAddress").setValue("Another address");
+        $("#submit").click();
+
+        $("#output").shouldBe(visible);
+        $("#name").shouldHave(text("Alina"));
+        $("#email").shouldHave(text("test@email.com"));
+        $("#output #currentAddress").shouldHave(text("Some address"));
+        $("#output #permanentAddress").shouldHave(text("Another address"));
+    }
+}
